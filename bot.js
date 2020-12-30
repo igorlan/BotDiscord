4138f01cd7b99bd7b2d001396f5453bbb3c4b9a3
igorlan <igor.lansarotto@gmail.com>
2020-12-30 19:55:37 -0300

    PORTA

--- bot.js
+++ bot.js
@@ -9,12 +9,7 @@ const Discord = require('discord.js'),
     };
 
     const PORT = process.env.PORT || 5000
-    express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .listen(PORT);
 
 // Create a new DisTube
 const distube = new DisTube(client, { searchSongs: false, emitNewSongOnly: true });
