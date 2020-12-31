d61a49ba20d2406ae866669243743cb602824378
igorlan <igor.lansarotto@gmail.com>
2020-12-30 21:08:17 -0300

    a

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const express = require('express');
 const http = require("http");
 const app = express();
 
-http.createServer(express).listen(5000, () => console.log("Servidor rodando local na porta 3000"));
+http.createServer(express).listen(80, () => console.log("Servidor rodando local na porta 0"));
 
 // DisTube example bot, definitions, properties and events details in the Documentation page.
 const Discord = require('discord.js'),
@@ -120,10 +120,7 @@ distube
         message.channel.send("An error encountered: " + e);
     });
 
-    app.listen(3000, function () {
-        console.log('Example app listening on port 3000!');
-    });
 
-    http.createServer(express).listen(5000, () => console.log("Servidor rodando local na porta 3000"));
+    http.createServer(express).listen(80, () => console.log("Servidor rodando local na porta 3000"));
 
 client.login(config.token);
