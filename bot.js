89f1e3745a7f1af9abe2797b969196949b59a45e
Renato <renato190698@gmail.com>
2020-12-31 01:10:10 -0300

    tirando filtro

--- bot.js
+++ bot.js
@@ -50,17 +50,16 @@ client.on("message", async (message) => {
   if (command == "jump") distube.jump(message, parseInt(args[0]));
 
   if (["tocaesselixoae", "play"].includes(command))
-  console.log('play: ', message);
-  console.log("args", args.join(" "));
-  for( let i = 0; i < neverPlaysThisSongs; i ++ ){
-    if (neverPlaysThisSongs[i] == args.join(" ")) {
-      message.channel.send(
-        "Só tocamos musica boa aqui.",
-      );
-      distube.stop(message);
-      return;
-    }
-  }
+
+  // for( let i = 0; i < neverPlaysThisSongs; i ++ ){
+  //   if (neverPlaysThisSongs[i] == args.join(" ")) {
+  //     message.channel.send(
+  //       "Só tocamos musica boa aqui.",
+  //     );
+  //     distube.stop(message);
+  //   }
+  //   return;
+  // }
     distube.play(message, args.join(" "));
 
   if (command == "loop") distube.setRepeatMode(message, parseInt(args[0]));
