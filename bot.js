86b8e894dc365fc63f70a4c64161a68df1c4721a
Renato <renato190698@gmail.com>
2020-12-31 00:21:20 -0300

    teste

--- bot.js
+++ bot.js
@@ -51,7 +51,8 @@ client.on("message", async (message) => {
   }
 
   if(command == "avancar"){
-    const numb = message.match(/\d/g)
+    // const numb = message.match(/\d/g)
+    console.log("avancar: ", message, Number(args[0]));
     distube.seek(message, Number(args[0]));
       message.channel.send(`Música avançada em ${Number(args[0])}.`);
   }
