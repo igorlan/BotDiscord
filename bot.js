1dccdde58a221ade56afc84f25ffadc4326c2fc7
igorlan <igor.lansarotto@gmail.com>
2020-12-30 23:57:03 -0300

    a

--- bot.js
+++ bot.js
@@ -35,11 +35,15 @@ client.on("message", async (message) => {
 
   if (command == "loop") distube.setRepeatMode(message, parseInt(args[0]));
 
-  if(command =="pausar")distube.pause(message);
+  if(command =="pausar") {
+      distube.pause(message);
   message.channel.send("Musica pausada, para retomar a música use o comando !retomar");
+  }
 
-  if(command == "retomar")distube.resume(message);
+  if(command == "retomar"){ 
+      distube.resume(message);
     message.channel.send("Musica retomada.");
+  }
 
   if (command == "stop") {
     distube.stop(message);
